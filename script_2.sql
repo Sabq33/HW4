e44fc1e6d1f2a306f228359e402a57b30e5dc00d
Dmitry Bychak <85137373+Sabq33@users.noreply.github.com>
2021-12-31 01:22:06 +0300

    Update_2

--- script_2.sql
+++ script_2.sql
@@ -1,16 +1,16 @@
 --SQL_DDL
---Первая часть.
 
---Таблица employees
 
---Создать таблицу employees
+-- 1.Таблица employees
+
+-- 2.Создать таблицу employees
 -- id. serial,  primary key,
 -- employee_name. Varchar(50), not null
 create table employees(
 	id serial primary key,
 	employee_name Varchar(50) not null
 );
---Наполнить таблицу employee 70 строками.
+-- 3.Наполнить таблицу employee 70 строками.
 insert into employees(employee_name)
 	values  ('Jason'),
 		    ('David'),
@@ -85,16 +85,16 @@ insert into employees(employee_name)
 
 delete from employees where employee_name = 'Jhon';	  
 
---Таблица salary
+-- 4.Таблица salary
 
---Создать таблицу salary
+-- 5.Создать таблицу salary
 -- id. Serial  primary key,
 --monthly_salary. Int, not null
 create table salary(
 	id serial primary key,
 	monthly_salary int not null
 );
---Наполнить таблицу salary 15 строками:
+-- 6.Наполнить таблицу salary 15 строками:
 insert into salary(monthly_salary)
 	values  (1000),
 			(1100),
@@ -112,10 +112,10 @@ insert into salary(monthly_salary)
             (2300),
             (2400);
             
---Таблица employee_salary
+-- 7.Таблица employee_salary
 
---Создать таблицу employee_salary
---- id. Serial  primary key,
+-- 8.Создать таблицу employee_salary
+-- id. Serial  primary key,
 -- employee_id. Int, not null, unique
 -- salary_id. Int, not null
 create table employee_salary(
@@ -123,8 +123,8 @@ create table employee_salary(
 	employee_id int not null unique,
 	salary_id int not null
 );
---Наполнить таблицу employee_salary 40 строками:
--- в 10 строк из 40 вставить несуществующие employee_id
+-- 9.Наполнить таблицу employee_salary 40 строками:
+-- 10. В 10 строк из 40 вставить несуществующие employee_id
 insert into employee_salary(employee_id, salary_id)
 	values (1,1),          
 		   (2,2),
@@ -167,19 +167,19 @@ insert into employee_salary(employee_id, salary_id)
 		   (53,9),
 		   (454,5);
 				
---Таблица roles
+-- 11.Таблица roles
 
---Создать таблицу roles
+-- 12.Создать таблицу roles
 --id. Serial  primary key,
 -- role_name. int, not null, unique
 create table roles(
 	id serial primary key,
 	role_name int not null unique
 );
---Поменять тип столба role_name с int на varchar(30)
+-- 13.Поменять тип столба role_name с int на varchar(30)
 alter table roles 
 alter column role_name type varchar(30);
---Наполнить таблицу roles 20 строками:
+-- 14.Наполнить таблицу roles 20 строками:
 	insert into roles(role_name)
 		values('Junior_Python_developer'),
 	          ('Middle_Python_developer'),
@@ -202,12 +202,12 @@ alter column role_name type varchar(30);
               ('Middle_Automation_QA_engineer'),
               ('Senior_Automation_QA_engineer');
 
---Таблица roles_employee
+-- 15.Таблица roles_employee
 
---Создать таблицу roles_employee
---- id. Serial  primary key,
---- employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
---- role_id. Int, not null (внешний ключ для таблицы roles, поле id)
+-- 16.Создать таблицу roles_employee
+-- id. Serial  primary key,
+-- employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
+-- role_id. Int, not null (внешний ключ для таблицы roles, поле id)
 create  table roles_employee(
 	id serial primary key,
 	employee_id int not null unique,
@@ -217,7 +217,7 @@ create  table roles_employee(
 	foreign key (role_id)
 		references roles(id)
 );
---Наполнить таблицу roles_employee 40 строками:
+-- 17. Наполнить таблицу roles_employee 40 строками:
 insert into roles_employee(employee_id, role_id)
 	values (1,1),
 	       (2,2),
